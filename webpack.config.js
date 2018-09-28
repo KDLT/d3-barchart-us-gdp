70b5cad0edccae8efb8900f31235668e69c9fdd2
KDLT <aguirrekenneth@gmail.com>
2018-09-28 18:47:58 +0800

    added clean webpack plugin, {dry: true} to not delete docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const htmlwp = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 const htmlWebpackPlugin = new htmlwp({
   // title: 'US GDP in Billions',
   favicon: './src/app/assets/favicon-slice-plated.ico',
   template: './src/index.html',
   filename: './index.html', // (output directory) no need to put dist
 });
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractPlugin = new ExtractTextPlugin({
-   filename: 'main.css'
+  filename: 'main.css'
 });
+
+const cleanwp = require('clean-webpack-plugin');
+const CleanWebpackPlugin = new cleanwp(['docs'], {dry: true})
 
 module.exports = {
   entry: './src/app/index.js',
@@ -40,6 +41,7 @@ module.exports = {
   plugins: [
     htmlWebpackPlugin,
     new UglifyJsPlugin(),
-    extractPlugin
+    extractPlugin,
+    CleanWebpackPlugin
   ]
 };
